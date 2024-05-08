3b053a4bb63e2217702915907aa0e68352719add
Anton Angelgardt <a.n.angelgardt@gmail.com>
2024-05-08 22:14:19 +0300

    basic deploy structure

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,3 +1,69 @@
 #!/bin/bash
 
 ## import functions and colors
+source deploy/modules/_set-colors.sh
+source deploy/modules/_render.sh
+source dpelo/modules/_deploy.sh
+source deploy/modules/_restore.sh
+source deploy/modules/_release.sh
+source deploy/modules/_logger.sh
+
+## make modes array
+
+modes=( "none" "render" "deploy" "release" "restore" )
+
+## assing inline args to vars, if no --- assign default
+
+mode="${1:-${modes[0]}}"
+
+## check mode
+## abort function if not valid
+
+mode_check="\<${mode}\>" # extract a regex that matches the exact value of the argument
+
+if [[ ! ${modes[@]} =~ $mode_check]]
+then
+
+  echo -e "${RED}\n=====\n=====${NC}"
+  echo -e "${RED}RUN FAILED${NC}"
+  echo -e "${RED}Unknown first inline argument${NC}"
+  echo -e "Valid options are ${GRAY}${modes[@]}${NC} (first is default)"
+  echo -e "${RED}\n=====\n=====\n${NC}"
+  exit
+  
+fi
+
+if [ "$mode" = "none" ]
+then
+
+  echo -e "${GREEN}\n=====\n=====${NC}"
+  echo -e "${GREEN}NOTHING DONE${NC}"
+  echo -e "${NC}none ${GREEN} mode set${NC}"
+  echo -e "${GREEN}\n=====\n=====\n${NC}"
+  exit
+  
+fi
+
+if [ "$mode" = "render" ]
+then
+  render 2>&1 | tee deploy/last.log
+fi
+
+if [ "$mode" = "deploy" ]
+then
+  deploy 2>&1 | tee deploy/last.log
+fi
+
+if [ "$mode" = "release" ]
+then
+  release 2>&1 | tee deploy/last.log
+fi
+
+if [ "$mode" = "restore" ]
+then
+  restore 2>&1 | tee deploy/last.log
+fi
+
+
+## add deployment info to docs/REAMDE.md
+logger # $mode
