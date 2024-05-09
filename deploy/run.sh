42b0e112eef355388c70e42c72fe446a12e8a18a
Anton Angelgardt <a.n.angelgardt@gmail.com>
2024-05-09 13:31:14 +0300

    update main flow for mac

--- deploy/run.sh
+++ deploy/run.sh
@@ -14,56 +14,44 @@ modes=( "none" "render" "deploy" "release" "restore" )
 ## assing inline args to vars, if no --- assign default
 mode="${1:-${modes[0]}}"
 
-## check mode
-## abort function if not valid
 
-mode_check="\<${mode}\>" # extract a regex that matches the exact value of the argument
+## main flow
+if [ "$mode" = "none" ]; then
 
-if [[ ! ${modes[@]} =~ $mode_check ]]
-then
+  echo -e "${GREEN}\n=====\n=====${NC}"
+  echo -e "${GREEN}NOTHING DONE${NC}"
+  echo -e "${NC}none ${GREEN}mode set${NC}"
+  echo -e "${GREEN}\n=====\n=====\n${NC}"
+  exit
+  
+elif [ "$mode" = "render" ]; then
+
+  echo "render mode"
+  # render 2>&1 | tee deploy/last.log
+
+elif [ "$mode" = "deploy" ]; then
+
+  echo "deploy mode"
+  # deploy 2>&1 | tee deploy/last.log
+
+elif [ "$mode" = "release" ]; then
+  echo "release mode"
+  # release 2>&1 | tee deploy/last.log
+
+elif [ "$mode" = "restore" ]; then
+
+  echo "restore mode"
+  # restore 2>&1 | tee deploy/last.log
+
+else
 
   echo -e "${RED}\n=====\n=====${NC}"
   echo -e "${RED}RUN FAILED${NC}"
-  echo -e "${RED}Unknown first inline argument${NC}"
+  echo -e "${RED}Unknown first inline argument value${NC} ${mode}"
   echo -e "Valid options are ${GRAY}${modes[@]}${NC} (first is default)"
   echo -e "${RED}\n=====\n=====\n${NC}"
   exit
   
-fi
-
-if [ "$mode" = "none" ]
-then
-
-  echo -e "${GREEN}\n=====\n=====${NC}"
-  echo -e "${GREEN}NOTHING DONE${NC}"
-  echo -e "${NC}none ${GREEN} mode set${NC}"
-  echo -e "${GREEN}\n=====\n=====\n${NC}"
-  exit
-  
-fi
-
-if [ "$mode" = "render" ]
-then
-  echo "render mode"
-  # render 2>&1 | tee deploy/last.log
-fi
-
-if [ "$mode" = "deploy" ]
-then
-  echo "deploy mode"
-  # deploy 2>&1 | tee deploy/last.log
-fi
-
-if [ "$mode" = "release" ]
-then
-  echo "release mode"
-  # release 2>&1 | tee deploy/last.log
-fi
-
-if [ "$mode" = "restore" ]
-then
-  echo "restore mode"
-  # restore 2>&1 | tee deploy/last.log
 fi
 
 
