7cd0d9d020b6566cc32ea9ef5c38f4d6f7a71744
Anton Angelgardt <a.n.angelgardt@gmail.com>
2024-05-10 16:26:03 +0300

    inject question text with js

--- book/_book/js/quiz.js
+++ book/_book/js/quiz.js
@@ -1,3 +1,36 @@
+var info_json = JSON.parse(quiz_json);
+
+console.log(info_json)
+
+let info = {};
+
+for (let i = 0; i < info_json.length; i++) {
+  info[info_json[i].name] = {};
+  for (let t = 1; t <= 20; t++) {
+    id = "q" + t;
+    info[info_json[i].name][id] = info_json[i][id];
+  }
+}
+
+console.log(info)
+
+const levels = document.getElementsByClassName("level");
+
+console.log(levels)
+console.log(levels)
+console.log(levels.length)
+
+for (let i = 1; i <= 20; i++) {
+  document.getElementById("lvl-q"+i).className += (" lvl-" + info["level"]["q"+i]);
+  document.getElementById("title-q"+i).innerHTML = info["text"]["q"+i];
+  document.getElementById("q"+i+"-option1").innerHTML = info["option1_label"]["q"+i];
+  document.getElementById("q"+i+"-option2").innerHTML = info["option2_label"]["q"+i];
+  document.getElementById("q"+i+"-option3").innerHTML = info["option3_label"]["q"+i];
+  document.getElementById("q"+i+"-option4").innerHTML = info["option4_label"]["q"+i];
+}
+
+console.log(levels)
+
 const submit = document.getElementById("submit-button");
 submit.addEventListener("click", check_quiz);
 
---
+++ dev/quiz/get_quiz_jsons.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(googlesheets4)
+
+quizes_names <- sheet_names("https://docs.google.com/spreadsheets/d/1lNWFJAZ5xOgqRxNUL2CjzTp5K7ErveR9BYF7CZCUXlo/edit?usp=sharing")
+quizes <- list()
+
+for (quiz_name in quizes_names) {
+  read_sheet(ss = "https://docs.google.com/spreadsheets/d/1lNWFJAZ5xOgqRxNUL2CjzTp5K7ErveR9BYF7CZCUXlo/edit?usp=sharing",
+             sheet = quiz_name,
+             col_types = "c",
+             skip = 2) -> quizes[[quiz_name]]
+}
+
+quizes[[1]] %>% 
+  mutate(
+    across(everything(), ~replace_na(.x, ""))
+  ) %>% 
+  mutate(
+    across(matches("^option\\d_correct$"), tolower)
+  ) %>% 
+  mutate(text = paste0(n, ". ", ques),
+         qn = paste0("q", n)) %>% 
+  pivot_longer(cols = -qn) %>% 
+  pivot_wider(names_from = qn,
+              values_from = value) %>% 
+  jsonlite::toJSON(dataframe = "rows") %>%
+  paste0("quiz_json='", ., "'", 
+         "\nquiz='quiz1'") %>% 
+  write(paste0("js/", "quiz1", ".json"))
+
+
+
+
+get_json <- function(hwn, hws_table) {
+  hws_table %>% 
+    filter(hw == hwn) %>%
+    mutate(
+      across(everything(), ~replace_na(.x, ""))
+    ) %>% 
+    select(-hw, -n) %>%
+    pivot_longer(cols = -task) %>% 
+    pivot_wider(names_from = task,
+                values_from = value) %>% 
+    jsonlite::toJSON(dataframe = "rows") %>%
+    paste0("hw_json='", ., "'", 
+           "\nN_TASKS=15",
+           "\nID='", hwn, "'") %>% 
+    write(paste0("js/", hwn, ".json"))
+}
+
+unique(hws_table$hw) %>% map(get_json, hws_table = hws_table)
